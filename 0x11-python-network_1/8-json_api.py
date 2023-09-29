d49640d2636001d898494661415557b1abd9f03f
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-09-29 15:06:29 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-Script that takes in a letter and sends a POST request to URL with letter param
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+sage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
+from sys import argv
+import requests
 
 
 if __name__ == "__main__":
-    from sys import argv
-    import requests
+    letter = "" if len(argv) == 1 else argv[1]
+    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
 
-    if len(argv) == 1:
-        q = ''
-    else:
-        q = argv[1]
-
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        out = r.json()
-        if out:
-            print('[{}] {}'.format(out['id'], out['name']))
+        response = req.json()
+        if response == {}:
+            print("No result")
         else:
-            print('No result')
-    except:
-        print('Not a valid JSON')
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
