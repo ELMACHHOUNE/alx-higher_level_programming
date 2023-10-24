cbe946834e4b5670e11e68b100b7eb320a65357a
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-10-24 15:29:02 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let fs = require('fs');
-let file = process.argv[2];
+// Reads and prints the content of a file
 
-function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-readPrint(file);
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
